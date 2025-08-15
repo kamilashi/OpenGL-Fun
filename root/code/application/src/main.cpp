ec786d8cf4e3700305fddd9c408f41719c296fe9
Kamila <kamillashirinova9r1@gmail.com>
2025-08-15 20:15:41 +0200

    updated gitignore, window render

--- root/code/application/src/main.cpp
+++ root/code/application/src/main.cpp
@@ -1,7 +1,54 @@
 #include <glad.h>
 #include <GLFW/glfw3.h>
 
+const struct WindowParams
+{
+	 float aspectRatio = 1.5f;
+};
+
 int main(int argc, char* argv[])
 {
-	
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+
+	const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+
+	WindowParams startupParams;
+	int width = static_cast<int>(mode->width / 2.0f);
+	int height = static_cast<int>(width/ startupParams.aspectRatio);
+
+	window = glfwCreateWindow(static_cast<int>(mode->width/2.0f), static_cast<int>(mode->height / 2.0f), "OpenGLApp", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		// failed to load OpenGL via GLAD
+		return -1;
+	}
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
